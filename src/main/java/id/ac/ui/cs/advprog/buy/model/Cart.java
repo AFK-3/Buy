cb0c1f6ef0eb245fd0f8ed31fc74b7eba247ee21
alif <malifalhakim11@gmail.com>
2024-04-17 18:44:09 +0700

    Add abstract ProductContainer class and its concrete class

---
+++ src/main/java/id/ac/ui/cs/advprog/buy/model/Cart.java
@@ -0,0 +1,9 @@
+package id.ac.ui.cs.advprog.buy.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Cart extends ProductContainer{
+    private String username;
+}
---
+++ src/main/java/id/ac/ui/cs/advprog/buy/model/Transaction.java
@@ -0,0 +1,8 @@
+package id.ac.ui.cs.advprog.buy.model;
+
+public class Transaction extends ProductContainer{
+    private String transactionId;
+    private String username;
+    private boolean status;
+
+}
