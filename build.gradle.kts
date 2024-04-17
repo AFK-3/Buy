cf267941bf1ed69ce42e0a4a68184fdd7622ec93
alif <malifalhakim11@gmail.com>
2024-04-17 07:40:35 +0700

    Deployment App

--- build.gradle.kts
+++ build.gradle.kts
@@ -81,9 +81,11 @@ tasks.test{
 	finalizedBy(tasks.jacocoTestReport)
 }
 
-tasks.jacocoTestReport{
-	reports {
-		xml.required = true
-	}
+tasks.jacocoTestReport {
 	dependsOn(tasks.test)
+	reports {
+		xml.required.set(true)
+		csv.required.set(true)
+		html.outputLocation.set(layout.buildDirectory.dir("jacocoHtml"))
+	}
 }
