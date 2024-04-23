67a29ffb73221c77970a365968f9d47300d6095f
alif <malifalhakim11@gmail.com>
2024-04-23 14:10:51 +0700

    Add TransactionStatus Enum

---
+++ src/main/java/id/ac/ui/cs/advprog/buy/enums/TransactionStatus.java
@@ -0,0 +1,26 @@
+package id.ac.ui.cs.advprog.buy.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum TransactionStatus {
+    WAITING_PAYMENT("WAITING_PAYMENT"),
+    FAILED("FAILED"),
+    SUCCESS("SUCCESS"),
+    CANCELLED("CANCELLED");
+
+    private final String value;
+
+    private TransactionStatus(String value){
+        this.value = value;
+    }
+
+    public static boolean contains(String param){
+        for (TransactionStatus orderStatus: TransactionStatus.values()){
+            if(orderStatus.name().equals(param)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
