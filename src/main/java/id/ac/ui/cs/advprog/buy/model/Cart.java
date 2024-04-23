55dfdeb1c5ce6e34a804340d9f34a494831355be
alif <malifalhakim11@gmail.com>
2024-04-23 19:16:47 +0700

    Modify Cart Model

--- src/main/java/id/ac/ui/cs/advprog/buy/model/Cart.java
+++ src/main/java/id/ac/ui/cs/advprog/buy/model/Cart.java
@@ -3,7 +3,15 @@ package id.ac.ui.cs.advprog.buy.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.HashMap;
+
 @Getter @Setter
-public class Cart extends ProductContainer{
+public class Cart {
     private String username;
+    private HashMap<String, Integer> listings;
+    private long totalPrice;
+
+    public Cart(String username){
+        this.username = username;
+    }
 }
