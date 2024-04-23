a4b8fb620caa466e09913012f6ff2aaeac7c1858
alif <malifalhakim11@gmail.com>
2024-04-23 13:42:42 +0700

    Modify methods in TransactionRepository

--- src/main/java/id/ac/ui/cs/advprog/buy/repository/TransactionRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/buy/repository/TransactionRepository.java
@@ -4,24 +4,31 @@ import id.ac.ui.cs.advprog.buy.model.ProductContainer;
 import id.ac.ui.cs.advprog.buy.model.Transaction;
 import org.springframework.stereotype.Repository;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 @Repository
 public class TransactionRepository extends ProductContainerRepository {
     private final Map<String, Transaction> transactionData = new HashMap<>();
 
     @Override
-    public Transaction filterByUser(String username) {
-        Optional<Transaction> transactionOptional = transactionData.values().stream()
-                .filter(transaction -> transaction.getUsername().equals(username))
-                .findFirst();
-
-        return transactionOptional.orElse(null);
+    public Transaction create(String identifier){
+        Transaction newTransaction = new Transaction();
+        transactionData.put(identifier,newTransaction);
+        return newTransaction;
     }
 
-    public Transaction filterById(String id){
+    @Override
+    public Transaction filterByIdentifier(String id){
         return transactionData.get(id);
+    }
+
+    public List<Transaction> filterByUser(String username){
+        List<Transaction> filteredTransaction = new ArrayList<>();
+        transactionData.forEach((key,value) -> {
+            if (value.getUsername().equals(username)){
+                filteredTransaction.add(value);
+            }
+        });
+        return filteredTransaction;
     }
 }
