98fd2e02adcacb0bd39184200b887b3f19f5f21e
alif <malifalhakim11@gmail.com>
2024-04-24 00:22:48 +0700

    Modify createTransaction method in Transaction Factory

--- src/main/java/id/ac/ui/cs/advprog/buy/model/TransactionFactory.java
+++ src/main/java/id/ac/ui/cs/advprog/buy/model/TransactionFactory.java
@@ -3,14 +3,13 @@ package id.ac.ui.cs.advprog.buy.model;
 import java.util.HashMap;
 
 public class TransactionFactory {
-    public static Transaction createTransaction(Cart cart, String transactionId, String status, String deliveryLocation){
+    public static Transaction createTransaction(Cart cart, String transactionId, String deliveryLocation){
         return new Transaction(
                 transactionId,
                 cart.getUsername(),
                 deliveryLocation,
                 new HashMap<>(cart.getListings()),
-                cart.getTotalPrice(),
-                status
+                cart.getTotalPrice()
         );
     }
 }
