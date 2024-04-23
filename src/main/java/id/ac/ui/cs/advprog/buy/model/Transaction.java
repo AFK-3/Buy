c6b961a71d3c6af2f6772af150224a988009d9b7
alif <malifalhakim11@gmail.com>
2024-04-23 19:17:16 +0700

    Modify Transaction Model and Implement Enum to Model

--- src/main/java/id/ac/ui/cs/advprog/buy/model/Transaction.java
+++ src/main/java/id/ac/ui/cs/advprog/buy/model/Transaction.java
@@ -1,12 +1,41 @@
 package id.ac.ui.cs.advprog.buy.model;
 
+import id.ac.ui.cs.advprog.buy.enums.TransactionStatus;
 import lombok.Getter;
-import lombok.Setter;
 
-@Getter @Setter
-public class Transaction extends ProductContainer{
+import java.util.HashMap;
+
+@Getter
+public class Transaction {
     private String transactionId;
     private String username;
-    private boolean status;
+    private String status;
     private String deliveryLocation;
+    private HashMap<String,Integer> listings;
+    private long totalPrice;
+
+    public Transaction(String transactionId, String username, String deliveryLocation, HashMap<String,Integer> listings, long totalPrice){
+        this.transactionId = transactionId;
+        this.username = username;
+        this.deliveryLocation = deliveryLocation;
+        if (listings.isEmpty()){
+            throw new IllegalArgumentException();
+        } else {
+            this.listings = listings;
+        }
+        this.totalPrice = totalPrice;
+    }
+
+    public Transaction(String transactionId, String username, String deliveryLocation, HashMap<String,Integer> listings, long totalPrice,String status){
+        this(transactionId, username, deliveryLocation, listings, totalPrice);
+        this.setStatus(status);
+    }
+
+    public void setStatus(String status){
+        if(TransactionStatus.contains(status)){
+            this.status = status;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
 }
