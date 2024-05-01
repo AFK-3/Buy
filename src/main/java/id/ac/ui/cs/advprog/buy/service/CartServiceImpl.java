00b225e935add480c67c7d122051668f374beb7a
alif <malifalhakim11@gmail.com>
2024-05-01 10:12:13 +0700

    refactor: modify updateTotalPrice method

--- src/main/java/id/ac/ui/cs/advprog/buy/service/CartServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/buy/service/CartServiceImpl.java
@@ -3,7 +3,14 @@ package id.ac.ui.cs.advprog.buy.service;
 import id.ac.ui.cs.advprog.buy.model.Cart;
 import id.ac.ui.cs.advprog.buy.repository.CartRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.HttpHeaders;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -14,6 +21,7 @@ import java.util.Map;
 public class CartServiceImpl implements CartService{
     @Autowired
     private CartRepository cartRepository;
+    private final String authUrl = "http://35.198.243.155/";
 
     @Override
     public Cart create(Cart cart) {
@@ -81,10 +89,34 @@ public class CartServiceImpl implements CartService{
     }
 
     @Override
-    public Cart updateTotalPrice(long newPrice, String username) {
-        // TODO : Hubungkan dengan microservice listings
+    public Cart updateTotalPrice(String username, String token) throws JSONException {
         Cart cart = cartRepository.findByUsername(username);
-        cart.setTotalPrice(newPrice);
+        Map<String,Integer> listings = cart.getListings();
+
+        String getListingIdUrl = authUrl + "/get-by-id/";
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization",token);
+        HttpEntity<String> entity = new HttpEntity<>("body",headers);
+        String jsonBody;
+        JSONObject jsonObject;
+
+        long totalPrice = 0;
+        for (Map.Entry<String, Integer> entry : listings.entrySet()){
+            String listingId = entry.getKey();
+            Integer quantity = entry.getValue();
+
+            ResponseEntity<String> response = restTemplate.exchange(getListingIdUrl + listingId, HttpMethod.GET,
+                    entity, String.class);
+
+            jsonBody = response.getBody();
+            jsonObject = new JSONObject(jsonBody);
+            Long price = jsonObject.getLong("price");
+
+            totalPrice = totalPrice + (price * quantity);
+        }
+
+        cart.setTotalPrice(totalPrice);
         return cartRepository.update(cart);
     }
 }
