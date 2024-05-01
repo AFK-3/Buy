c979ad4a7258abfddf4709661722e88f40cd25d6
alif <malifalhakim11@gmail.com>
2024-05-01 10:14:32 +0700

    refactor: modify create method in transaction service

--- src/main/java/id/ac/ui/cs/advprog/buy/service/TransactionService.java
+++ src/main/java/id/ac/ui/cs/advprog/buy/service/TransactionService.java
@@ -1,11 +1,12 @@
 package id.ac.ui.cs.advprog.buy.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import id.ac.ui.cs.advprog.buy.model.Transaction;
 
 import java.util.List;
 
 public interface TransactionService {
-    public Transaction create(Transaction transaction);
+    public Transaction create(Transaction transaction,String token) throws JsonProcessingException;
     public Transaction findById(String transactionId);
     public List<Transaction> findByUsername(String username);
     public List<Transaction> findAll();
--- src/main/java/id/ac/ui/cs/advprog/buy/service/TransactionServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/buy/service/TransactionServiceImpl.java
@@ -1,22 +1,47 @@
 package id.ac.ui.cs.advprog.buy.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import id.ac.ui.cs.advprog.buy.model.Transaction;
 import id.ac.ui.cs.advprog.buy.repository.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class TransactionServiceImpl implements TransactionService{
     @Autowired
     private TransactionRepository transactionRepository;
+    private final String authUrl = "http://35.198.243.155/";
 
     @Override
-    public Transaction create(Transaction transaction) {
+    public Transaction create(Transaction transaction,String token) throws JsonProcessingException {
         transactionRepository.create(transaction);
+
+        RestTemplate restTemplate = new RestTemplate();
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        Map<String, Object> paymentRequest = new HashMap<>();
+        paymentRequest.put("id",transaction.getTransactionId());
+        paymentRequest.put("paymentStatus", "WAITING_RESPONSE");
+        paymentRequest.put("paymentAmount",transaction.getTotalPrice());
+        paymentRequest.put("buyerUsername",transaction.getUsername());
+
+        String jsonRequest = objectMapper.writeValueAsString(paymentRequest);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization",token);
+        headers.set("Content-Type", "application/json");
+        HttpEntity<String> entity = new HttpEntity<>(jsonRequest, headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(
+                authUrl + "/payment-request/create", HttpMethod.POST, entity, String.class);
+
         return transaction;
     }
 
